573c4ce2aae9198f852a096a8cf32da8bf988021
weeknd <weeknd.su@hotmail.com>
2018-11-26 19:41:27 +0800

    Update datachart.py

--- docs/python/datachart.py
+++ docs/python/datachart.py
@@ -1,3 +1,31 @@
+# Pyecharts对文本数据画图
+
+
+
+## Before You Start
+---
+
+!!! Warning ""
+    已安装 **python3** 或者 **Above** ，这里操作为 **LINUX**。**Windows** 不做解释。
+
+#### 1. 安装依赖包
+```Bash tab="Bash"
+yum install wget
+pip install re
+pip install pyecharts
+```
+
+!!! Failure ""
+    确认你的 **pip** 为安装到 **python3** 下，而不是自带的 **python2.7**
+
+#### 2. 获取Demo文件
+   [Click Me](https://raw.githubusercontent.com/weekndchina/mkdocs/master/docs/Demofile/1.txt)
+   ```
+   wget https://raw.githubusercontent.com/weekndchina/mkdocs/master/docs/Demofile/1.txt
+   ```
+
+#### 3. 编写python文件
+```Bash tab="demo.py"
 import re
 from pyecharts import Bar,Line,Overlap,Grid
 with open('1.txt','r') as f:
@@ -34,4 +62,8 @@ overlap = Overlap(width=1200, height=700)
 overlap.add(line1)
 overlap.add(line2, yaxis_index=1, is_add_yaxis=True)
 overlap.render()
+```
 
+
+#### 4. 效果图
+![效果图](https://raw.githubusercontent.com/weekndchina/mkdocs/master/docs/Demofile/echarts.png)
